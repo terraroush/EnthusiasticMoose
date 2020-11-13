1372e2b78febb06fb0f023e88c0edb1b9b279016
Terra Roush <terra.roush@gmail.com>
2020-11-13 10:36:32 -0600

    phase 7

--- Program.cs
+++ Program.cs
@@ -13,6 +13,15 @@ namespace EnthusiasticMoose
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
 
+            CanadaQuestion();
+            EnthusiasticQuestion();
+            LoveCSharpQuestion();
+            SecretQuestion();
+
+        }
+
+        static void CanadaQuestion()
+        {
             bool isTrue = MooseAsks("Is Canada real?");
             if (isTrue)
             {
@@ -21,6 +30,45 @@ namespace EnthusiasticMoose
             else
             {
                 MooseSays("I  K N E W  I T !!!");
+            }
+        }
+
+        static void EnthusiasticQuestion()
+        {
+            bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+            if (isEnthusiastic)
+            {
+                MooseSays("Yay!");
+            }
+            else
+            {
+                MooseSays("You should try it!");
+            }
+        }
+
+        static void LoveCSharpQuestion()
+        {
+            bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+            if (doesLoveCSharp)
+            {
+                MooseSays("Good job sucking up to your instructor!");
+            }
+            else
+            {
+                MooseSays("You will...oh, yes, you will...");
+            }
+        }
+
+        static void SecretQuestion()
+        {
+            bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+            if (isEverythingFine)
+            {
+                MooseSays("ME TOO!!!! I love secrets...tell me one!");
+            }
+            else
+            {
+                MooseSays("Oh, no...secrets are the best, I love to share them!");
             }
         }
 
