5d04250c35a70bf7cf746b6ff161d600845f858f
Terra Roush <terra.roush@gmail.com>
2020-11-13 10:27:40 -0600

    phase 5

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace EnthusiasticMoose
 
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+            bool isTrue = MooseAsks("Is Canada real?");
+            Console.WriteLine(isTrue);
         }
 
         static void MooseSays(string message)
@@ -44,6 +47,26 @@ namespace EnthusiasticMoose
                       \ \'._  ` '_.'
                        `^^` `^^^`
             ");
+        }
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
