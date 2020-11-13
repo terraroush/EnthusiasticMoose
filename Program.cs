e6d91d99efe574b47f3e0c3d13fdb2952515fcee
Terra Roush <terra.roush@gmail.com>
2020-11-13 10:17:25 -0600

    phase 4

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace EnthusiasticMoose
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine();
+
+            MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            MooseSays("I really am enthusiastic");
         }
 
         static void MooseSays(string message)
         {
-            Console.WriteLine(@"
+            Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
